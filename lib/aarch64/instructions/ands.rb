ae493dee64f09dea0433f25a561961fb6fef9a15
Aaron Patterson <tenderlove@ruby-lang.org>
2024-05-22 09:59:25 -0700

    oops

---
+++ lib/aarch64/instructions/ands.rb
@@ -0,0 +1,8 @@
+module AArch64
+  module Instructions
+    module ANDS
+      autoload :LOG_imm, "aarch64/instructions/ands/log_imm"
+      autoload :LOG_shift, "aarch64/instructions/ands/log_shift"
+    end
+  end
+end
